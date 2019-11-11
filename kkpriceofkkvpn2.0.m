523fb1fb7bf6a666237bca55cd58b2a6489bcf4b
Jim <45321804+a2zitpro@users.noreply.github.com>
2019-11-11 20:32:57 +0800

    Update kkpriceofkkvpn2.0.m

--- kkpriceofkkvpn2.0.m
+++ kkpriceofkkvpn2.0.m
@@ -2,5 +2,6 @@
 答：仍在免费公测期，尚无价格
 
 #### 推荐阅读
-- [关于金刚梯价格等](https://a2zitpro.github.io/web/列表-金刚梯价格)
-- [问题与解答](https://a2zitpro.github.io/web/列表-问题与解答)
+- [金刚梯价格](https://a2zitpro.github.io/web/list_kkprice)
+- [金刚帮助](https://a2zitpro.github.io/web/list_helpkkvpn)
+- [金刚梯](https://a2zitpro.github.io/web/dlb)
